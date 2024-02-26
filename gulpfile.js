6116fe910a131a6c07e1ab6372216526fa8d869f
Develop Bencosme Diaz <benjaminbencosme237@gmail.com>
2024-02-26 14:33:47 -0400

    Seccion Festiva e Conversion de imagenes finalizada

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, parallel} = require("gulp");
+
+//Dependencias de CSS:
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
 
+//Dependencia de Imagenes:
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     src('src/scss/**/*.scss') //Identificar el archivo de SASS
@@ -12,6 +19,42 @@ function css(done){
     done(); //Callback avisa cuando Gulp Llega al final
 }
 
+function imagenes(done){
+
+    const opciones ={
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{jpg,png}')
+        .pipe( cache(imagemin(opciones)) )
+        .pipe(dest('build/img'))    
+    done();
+}
+
+function versionWebp(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{jpg,png}')
+        .pipe( webp(opciones) )
+        .pipe(dest('build/img'))
+
+
+    done();
+}
+
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{jpg,png}')
+        .pipe( avif(opciones) )
+        .pipe(dest('build/img'))
+
+
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss', css);
 
@@ -19,4 +62,7 @@ function dev(done){
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
